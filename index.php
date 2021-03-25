40730f327b29c652479bbaaf0ff87df4e25c3429
Heather Lynn <heatherapellynn@gmail.com>
2021-03-25 06:51:51 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,373 @@
+<! DOCTYPE>
+<html style="scroll-behavior:smooth !important" lang="en">
+<head>
+<meta property="og:image" content="https://sitesappsimages.com/assets/images/thumb.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/png" href="https://www.sitesappsimages.com/talis/assets/icons/logo_icon.png">
+<style>
+
+//@media screen and (min-width: 30em) and (orientation: landscape) {section { padding-top: 400px; }}
+@media (min-width: 360px) and (orientation: portrait) {section { padding-top: 5em; }}
+@media (min-width: 375px) and (orientation: portrait) {section { padding-top: 8em; }}
+@media (min-width: 568px) and (orientation: landscape) {section { padding-top: 5em; }}
+@media (min-width: 667px) and (orientation: landscape) {section { padding-top: 5em; }}
+@media (min-width: 736px) and (orientation: landscape) {section { padding-top: 5em; }}
+@media (min-width: 812px) and (orientation: landscape) {section { padding-top: 6em; }}
+@media (min-width: 1024px) and (orientation: portrait) {section { padding-top: 5em; }}
+@media (min-width: 1024px) and (orientation: landscape) {section { padding-top: 6em; }}
+@media (min-width: 1366px) and (orientation: landscape) {section { padding-top: 6em; }}
+//@media screen and (min-width: 30em) and (orientation: portrait) { section { padding-top: 5em; } }
+//@media (min-width: 780px) { section { padding-top: 215px; } }
+//@media (min-width: 980px) { section { padding-top: 120px; } }
+//@media (min-width: 1350px) { section { padding-top: 180px; } }
+body {
+	height:100%;
+	margin:auto;
+}
+
+
+.icon-bar {
+  position: fixed;
+	z-index:99999;
+  top:	55%;
+	right: 25px;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+.top-container {
+  background-color: #232323;
+  padding: 0px;
+  font-weight:500;
+  text-align: center;
+  z-index: 8888;
+  width:100%;
+}
+
+.header {
+  padding: 10px 16px;
+  background: #fff;
+  border-bottom: 5px solid silver;
+  color: #f1f1f1;
+  z-index: 8888;
+}
+
+.navbar {
+ color: #f1f1f1;
+}
+
+.navbar li {
+ padding-left:.7em;
+ padding-right:.7em;
+}
+
+.content {
+  padding: 16px;
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.sticky + .content {
+  //padding-top: 102px;
+}
+img {
+	max-width:100%
+}
+.bg-custom {
+    background-color:transparent;
+}
+
+/* Fixed/sticky icon bar (vertically aligned 50% from the top of the screen) */
+.icon-bar {
+  position: fixed;
+	z-index:99999;
+  top:	55%;
+	right: 0;
+  -webkit-transform: translateY(-50%);
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+
+/* Style the icon bar links */
+.icon-bar a {
+  display: block;
+  text-align: center;
+  padding: 11px;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 20px;
+}
+
+/* Style the social media icons with color, if you want */
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.google {
+  background: #dd4b39;
+  color: white;
+}
+
+.linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.youtube {
+  background: #bb0000;
+  color: white;
+}
+.img-fluid-height {
+  max-height: 100%;
+  object-fit: contain;
+  overlow:hidden;
+  padding:8px;
+}
+
+
+ #l1 {
+ display:none;
+ }
+ #l2 {
+ display:none;
+ }
+ #l3 {
+ display:none;
+ }
+ #l4 {
+ display:none;
+ }
+ #l5 {
+ display:none;
+ }
+ #l6 {
+ display:none;
+ }
+ #l7 {
+ display:none;
+ }
+ #l8 {
+ display:none;
+ }
+
+
+
+</style>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+</head>
+<body>
+
+
+
+<?php
+include('nav.php');
+?>
+
+<table style="height:100%;">
+<tr style="height:100%;"><td style="height:100%;vertical-align:text-top;">
+
+
+		<?php
+		//grabbing catlinks
+		$res3 = $mysqli->query("select * from catlinks where delrec != 'on' and siteid='$siteid'");
+		if($myrow3=$res3->fetch_object()){
+		echo '<dl style="padding-left:2em;">';
+		$sidenav1=$myrow3->sidenav1;
+		$sidenav2=$myrow3->sidenav2;
+		$sidenav3=$myrow3->sidenav3;
+		$sidenav4=$myrow3->sidenav4;
+		$sidenav5=$myrow3->sidenav5;
+		$sidenav6=$myrow3->sidenav6;
+		$sidenav7=$myrow3->sidenav7;
+		$sidenav8=$myrow3->sidenav8;
+		$sidenav9=$myrow3->sidenav9;
+		$sidenav10=$myrow3->sidenav10;
+		$sidenav11=$myrow3->sidenav11;
+		$sidenav12=$myrow3->sidenav12;
+		$sidenav13=$myrow3->sidenav13;
+		$sidenav14=$myrow3->sidenav14;
+		$sidenav15=$myrow3->sidenav15;
+
+		for($count = 1; $count < 16; $count++) {
+		$thissidenav = 'sidenav' . $count;
+
+
+					$res2=$mysqli->query("select * from categories where navid='$thissidenav' and delrec != 'on' and siteid='$siteid'");
+					if($myrow2=$res2->fetch_object()){
+								printf("<dt><a id='sidemenu' href='#'>%s</a></dt>", $$thissidenav);
+								echo "<dd><ul>";
+										do{
+										printf("<li><a target='_blank' id='sidemenu' href='%s'>%s</a></li>", $myrow2->cat_url, $myrow2->catname);
+										//printf("<form method='post' action='%s'><li><a target='_blank' id='sidemenu' href='%s'>%s</a></li><input type='hidden' name='issubmitted' value='1'></form>", $myrow2->cat_url, $myrow2->cat_url, $myrow2->catname);
+										}while($myrow2=$res2->fetch_object());
+								echo "</ul></dd>";
+
+					}
+
+		}
+		echo "</dl>";
+		//grabbing weblinks
+		} else { echo $siteid; }
+		?>
+
+</td><td style="height:100%;vertical-align:text-top;">
+		<?php
+		include('grab_index.php');
+		?>
+</td></tr></table>
+<div style="position:fixed;bottom:2px;width:100%;text-align:center;"><font color=silver><small>All Rights Reserved&nbsp;&nbsp;&copy;2020 The App Factory</small></font></div>
+
+<!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script>
+$(document).ready(function() {
+	//alert("hey");
+		$('#menuButton').click(function(){
+		$("#imgBrand").hide();
+  });
+});
+
+
+</script>
+
+<script>
+$( "#imgBrand" ).click(function() {
+  $( "#home" ).show();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+
+$( "#link1" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).show();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+
+$( "#link2" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).show();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+
+$( "#link3" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).show();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+$( "#link4" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).show();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+$( "#link5" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).show();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+$( "#link6" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).show();
+  $( "#l7" ).hide();
+  $( "#l8" ).hide();
+
+});
+$( "#link7" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).show();
+  $( "#l8" ).hide();
+
+});
+$( "#link8" ).click(function() {
+  $( "#home" ).hide();
+  $( "#l1" ).hide();
+  $( "#l2" ).hide();
+  $( "#l3" ).hide();
+  $( "#l4" ).hide();
+  $( "#l5" ).hide();
+  $( "#l6" ).hide();
+  $( "#l7" ).hide();
+  $( "#l8" ).show();
+
+});
+</script>
+
+</body>
+</div>
+</html>
---
+++ nav.php
@@ -0,0 +1,58 @@
+<?php
+require('comm_connect.php');
+include('siteid.php');
+//grabbing weblinks
+if($res3 = $mysqli->query("select * from weblinks where delrec != 'on' and siteid='$siteid'")){
+$width='100%';
+$row3=$res3->fetch_object();
+$linkColor=$row3->linkColor;
+$link1=$row3->link1;
+$link2=$row3->link2;
+$link3=$row3->link3;
+$link4=$row3->link4;
+$link5=$row3->link5;
+$link6=$row3->link6;
+$link7=$row3->link7;
+$link8=$row3->link8;
+?>
+<div class="container-fluid"><nav class="header sticky-top navbar navbar-expand-lg navbar-light bg-light">
+	  <button id="menuButton" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		<span><img src="assets/images/logo_white.png" width="40" /></span>
+	  </button>
+	  <div class="collapse navbar-collapse" data-toggle="collapse" data-target=".navbar-collapse.show"  id="navbarTogglerDemo01">
+	  <li><a id="imgBrand" class="navbar-brand nav-link active" href="index.php"><img src="assets/images/logo_white.png" width="70" /><span class="sr-only">(current)</span></a></li>
+	   <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+		  <li class="nav-item">
+		    <a id="link1" class="nav-link" href="#link1"><?php echo $link1; ?></a>
+		  </li>
+		  <li class="nav-item">
+		    <a id="link2" class="nav-link" href="#link2"><?php echo $link2; ?></a>
+		  </li>
+		<li class="nav-item">
+			<a id="link3" class="nav-link" href="#link3"><?php echo $link3; ?></a>
+		  </li>
+		  <li class="nav-item">
+			<a id="link4" class="nav-link" href="#link4"><?php echo $link4; ?></a>
+		  </li>
+		  <li class="nav-item">
+		   <a id="link5" class="nav-link" href="#link5"><?php echo $link5; ?></a>
+		  </li>
+		  <li class="nav-item">
+		  <a id="link6" class="nav-link" href="#link6"><?php echo $link6; ?></a>
+		  </li>
+		<li class="nav-item">
+			<a id="link7" class="nav-link" href="#link7"><?php echo $link7; ?></a>
+		  </li>
+		  <li class="nav-item">
+			<a id="link8" class="nav-link" href="#link8"><?php echo $link8; ?></a>
+		  </li>
+		</ul>
+	  </div> <!-- navbar-->
+
+	  </div> <!-- navbar-->
+	</nav>
+
+<?php
+} else { echo "Error grabbing primary links."; }
+?>
