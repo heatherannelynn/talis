e17cafde9e1ea4a17449a876d074c421573f7fd7
Heather Lynn <heatherapellynn@gmail.com>
2021-03-28 11:46:56 -0400

    Add files via upload

--- comm_connect.php
+++ comm_connect.php
@@ -1,14 +1,11 @@
 <?php
-$mysqli = new mysqli('mysql01-dc3.mycorphosting.com:3306', 'commulis_user', 'Wjy42@x9', 'commulis_');
-
-//$mysqli = new mysqli('commulistbiz.startlogicmysql.com', 'heather', 'NewportShorts5!', 'commulist2');
-//$mysqli = new mysqli('localhost', 'root', '', 'testimports');
+$mysqli = new mysqli('localhost', 'root', '', 'testDB');
 if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
 }
 
-//echo 'Success... ' . $mysqli->host_info . "\n";
+echo 'Success... ' . $mysqli->host_info . "\n";
 
-//$mysqli->close();
+$mysqli->close();
 ?>
