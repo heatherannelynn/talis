44fc5954ce6cf781d1fa0f307be14a738a2b2630
Heather Lynn <heatherapellynn@gmail.com>
2021-04-09 11:12:03 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -247,7 +247,37 @@ $link8=$row3->link8;
                 <!-- Contains the main content of the webpage-->
                 <div class='col-10' id='grab_index'>
                    <?php
-                   include('grab_index.php');
+                  for($i=0;$i<9;$i++){
+$flag="link".$i;
+$linkDiv="l".$i;
+//echo $flag;
+$sql="SELECT * FROM classifieds where siteid='".$siteid."' and delrec != 'on' and ".$flag." = 'on'";
+if($res11 = $mysqli->query($sql)){
+	if($row11=$res11->fetch_object()){
+		//echo '<div style="padding-top:120px;"></div>';
+	echo "<div id='".$linkDiv."' class='content'>";
+
+		do{
+		echo "<div class='row'><div class='col'>";
+			if(isset($row11->authname)) { $authname= $row11->authname; }
+  			if(isset($row11->embed_code)) {$embed_code = $row11->embed_code; }
+  			if(isset($row11->title)) {$title = $row11->title; }
+  			if(isset($row11->thislisting)) {$thislisting = $row11->thislisting; }
+  			if(!isset($row11->image_name)) {$image_name = $row11->image_name; } else { $image_name="https://www.sitesappsimages.com/talis/assets/images/logo_white.png";}
+  			printf('<h4>%s</h4>',$title);
+  			printf('<p class="lead">%s</p>',$i);
+			printf('<div class="float-left"><img class="img-fluid-height" src="%s" /></div>',$image_name);
+  			//printf('<p class="para">%s</p><object>%s</object>',$thislisting, $embed_code);
+  			printf('<p class="para">%s</p>',$thislisting);
+		echo "</div></div>";
+		}while($row11=$res11->fetch_object());
+  	echo "</div>";
+	} 
+}
+		
+
+}
+
                    ?>
                 </div>
             </div><!-row-->
